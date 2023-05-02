3bc49d1ef61fe8421c05831fc7e0240da4d390ae
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-05-02 18:20:22 +0530

    Text from component is created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./Components/Header/Header"
+import TextForm from "./Components/TextForm/TextForm"
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <Header />
+      <TextForm />
 
     </>
   )
---
+++ client/src/Components/TextForm/TextForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import { Grid, Button } from '@mui/material';
+import { useState } from 'react';
+const style = {
+    position: 'absolute',
+    top: '23%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    p: 4,
+};
+function TextForm() {
+    const [user, setUser] = useState({
+        name: "",
+    });
+
+    // onchange function
+    const handleInputChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+        console.log(user)
+    }
+
+    // Submit function
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const { name } = user;
+
+        console.log(user)
+    }
+    return (
+        <Box sx={style}>
+
+            <TextField id="standard-basic" name="name" label="Type your task" variant="standard" fullWidth value={user.name} onChange={handleInputChange} />
+
+            <Grid container justifyContent="center" marginTop={4}>
+                <Button variant="outlined" style={{ background: "#EC401B", color: "white", border: "none" }} onClick={handleSubmit}>
+                    Submit
+                </Button>
+            </Grid>
+        </Box>
+    )
+}
+
+export default TextForm
