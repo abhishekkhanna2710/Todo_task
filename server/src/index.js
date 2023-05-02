ddda0c0814be3ca7ea814d407f63804fbb44c8b1
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-05-02 18:29:42 +0530

    Index file craeted

---
+++ server/src/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+
+// middlewares
+app.use(express.json())
+app.use(cors());
+
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World from Abhishek");
+})
+
+app.listen(5000, () => {
+    console.log(`Server has started on http://localhost:${port}`)
+})
